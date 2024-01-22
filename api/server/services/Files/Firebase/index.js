c83118a7c1e60fba5975b74e8fc8b51a94359100
17x-x <3303002880@qq.com>
2024-01-22 12:39:51 +0800

    仓库已清空

--- api/server/services/Files/Firebase/index.js
+++
@@ -1,7 +0,0 @@
-const images = require('./images');
-const initialize = require('./initialize');
-
-module.exports = {
-  ...images,
-  ...initialize,
-};
--- client/src/components/Nav/SettingsTabs/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as General } from './General/General';
-export { ClearChatsButton } from './General/General';
-export { default as Data } from './Data/Data';
-export { RevokeKeysButton } from './Data/Data';
-export { default as Account } from './Account/Account';
